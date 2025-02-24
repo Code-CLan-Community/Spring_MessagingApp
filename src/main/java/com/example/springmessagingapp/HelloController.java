9fa39dc06bd554ddb75e2c28f0dc53180b966dde
Code-CLan-Community <veergupta0801@gmail.com>
2025-02-24 11:58:57 +0530

    [Rishabh Gupta] Add. uc2 Get request using Query Parameter

---
+++ src/main/java/com/example/springmessagingapp/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.springmessagingapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    // GET Request with Query Parameter
+    @GetMapping("/query")
+    public String sayHello(@RequestParam(value = "name", defaultValue = "User") String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+}
